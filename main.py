573d7f9c958b266f9668649a5f87c5388871dedb
Joshua Liddell <joshua@Joshuas-MacBook-Pro-20.local>
2024-10-23 21:43:18 -0600

    Added analyze button

--- main.py
+++ main.py
@@ -13,13 +13,33 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Text File Analyzer")
         self.setFixedSize(QSize(700, 450))
 
-
+        # Widgets
         self.label = QLabel("Please select a text file for analysis")
+        font = QFont()
+        font.setPointSize(18)
+        self.label.setFont(font)
 
         self.fileselect = QPushButton("Browse Files")
         self.fileselect.clicked.connect(self.startButtonClicked)
         self.fileselect.setFixedSize(150, 30)
-        
+        self.analyze = QPushButton("Analyze File")
+        self.analyze.clicked.connect(self.analyzeButtonClicked)
+        self.analyze.setFixedSize(150, 30)
+        self.analyze.setStyleSheet("""
+                    QPushButton {
+                        background-color: #0056b3; 
+                        color: white; 
+                        border: 1px solid transparent; /* Keeps the original border */
+                        border-radius: 5px; /* Adjust this value for rounded corners */
+                        padding: 5px; /* Add some padding for size consistency */
+                    }
+                    QPushButton:hover {
+                        background-color: #0056b3; /* Darker blue on hover */
+                    }
+                """)
+
+        self.analyze.setVisible(False)
+
 
         # --- IN progress ---
 
@@ -29,6 +49,7 @@ class MainWindow(QMainWindow):
         self.layout.addStretch()
         self.layout.addWidget(self.label, alignment=Qt.AlignCenter)
         self.layout.addWidget(self.fileselect, alignment=Qt.AlignCenter)
+        self.layout.addWidget(self.analyze, alignment=Qt.AlignCenter)
         self.layout.addStretch()
 
 
@@ -43,9 +64,23 @@ class MainWindow(QMainWindow):
 
     def startButtonClicked(self):
         print("Button clicked!")
-        file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*)")
+        file_path, _ = QFileDialog.getOpenFileName(self, "Open File", os.path.expanduser("~/Downloads"), "Text Files (*.txt)")
+
         if file_path:
-            print("File "+os.path.basename(file_path)+" successfuly loaded")
+            userfile = open(file_path, 'r')
+            self.content = userfile.read()
+            self.label.setText("File successfully loaded")
+            self.fileselect.setText("Select different file")
+            self.label.setStyleSheet("color: green;") 
+            self.analyze.setText(f"Analyze {os.path.basename(file_path)}")
+            self.analyze.setVisible(True)
+        else:
+            print("Failed to find filepath")
+    
+    def analyzeButtonClicked(self):
+        print("analyzing... Here is the file content: ")
+        print(self.content)
+    
 
     def closeEvent(self, event):
         print("Application closed")
